dced35d0cf3898fdb6f80d40a141f211d660af09
Cwelshofer1 <sweetbrooo@gmail.com>
2025-04-18 14:36:21 -0500

    Homepage Listing all dogs

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,7 +1,9 @@
 import { getGreeting } from "./apiManager";
+import { getDogs } from "./Dogs";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [dogs, setDogs] = useState()
   const [greeting, setGreeting] = useState({
     message: "Not Connected to the API",
   });
@@ -14,5 +16,24 @@ export default function Home() {
       });
   }, []);
 
-  return <p>{greeting.message}</p>;
-}
+  useEffect(() => {
+    getDogs()
+      .then(setDogs)
+      });
+   [];
+
+
+  return <p>{greeting.message}</p>,
+<ul>
+  {dogs.map(d => (
+    <li key={d.id}>{d.name}</li>
+  ))}
+</ul>
+  }
+  
+
+  
+  
+
+  
+
